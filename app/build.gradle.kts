b94072d20472ecdc540e8d9eb25d316688b49b7b
LewanMordor@protonmail.com <LewanMordor@protonmail.com>
2024-02-28 20:31:27 +0100

    Add new theme (AppTheme) to application (dark theme). Add button design. change UI styles.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,7 +52,7 @@ android {
 }
 
 dependencies {
-
+    api("com.google.android.material:material:1.11.0")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
@@ -71,4 +71,5 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
 }
